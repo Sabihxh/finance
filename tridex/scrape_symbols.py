91bd51a2cfa557228b29584645a7a5495758caed
Sabih <sabihhasan12@hotmail.co.uk>
2019-06-02 13:10:23 +0100

    adds functionality to scrape symbol from more  uk indices

--- tridex/scrape_symbols.py
+++ tridex/scrape_symbols.py
@@ -1,3 +1,5 @@
+import os
+
 import requests
 from lxml import html
 
@@ -5,59 +7,48 @@ from lxml import html
 If this file is run directly then saves ftse350 stock codes in 
 a file. 
 Note: This will override the existing file
+
+
+Url for any index on lse website is the base_index_url + suffix 
+
 """
+base_index_url = 'https://www.londonstockexchange.com/exchange/prices-and-markets/stocks/indices/summary/summary-indices-constituents.html?index='
+suffix_index_urls = {
+	'FTSE 100': ('UKX&page=', 6), # (url, no of pages)
+	'FTSE 250': ('MCX&page=', 13),
+	'FTSE 350': ('NMX&page=', 18),
+	'FTSE ALL-SHARE': ('ASX&page=', 32),
+	'FTSE AIM UK 50': ('AIM5&page=', 3),
+	'FTSE AIM 100': ('AIM1&page=', 6),
+	'FTSE AIM ALL-SHARE': ('AXX&page=', 39),
+}
 
-def ftse350_stock_codes():
+
+def scrape_stock_symbols(index):
 	"""
 	Scrapes stock codes from london stock exchange website
 	and returns list of codes
 	"""
-	stock_codes = []
-	base_url = "https://www.londonstockexchange.com/exchange/prices-and-markets/stocks/indices/summary/summary-indices-constituents.html?index=NMX&page={}"
-	for x in range(1, 19):
-		url = base_url.format(x)
+	result = []
+	url_suffix = suffix_index_urls[index][0]
+	last_page_no = suffix_index_urls[index][1]
+
+	for x in range(1, last_page_no + 1):
+		url = base_index_url + url_suffix + str(x)
 		print('Scraping: {}'.format(url))
 		page = requests.get(url)
 		tree = html.fromstring(page.content)
-		codes = tree.xpath('//td[@scope="row" and @class="name"]/text()')
-		stock_codes.extend(codes)
-	return stock_codes
-
-
-def ftse100_stock_codes():
-	"""
-	Scrapes stock codes from london stock exchange website
-	and returns list of codes
-	"""
-	stock_codes = []
-	base_url = "https://www.londonstockexchange.com/exchange/prices-and-markets/stocks/indices/summary/summary-indices-constituents.html?index=UKX&page={}"
-	for x in range(1, 7):
-		url = base_url.format(x)
-		print('Scraping: {}'.format(url))
-		page = requests.get(url)
-		tree = html.fromstring(page.content)
-		codes = tree.xpath('//td[@scope="row" and @class="name"]/text()')
-		stock_codes.extend(codes)
-	return stock_codes
-
+		symbols = tree.xpath('//td[@scope="row" and @class="name"]/text()')
+		result.extend(symbols)
+	return result
 
 
 if __name__ == '__main__':
-	stock_codes = ftse350_stock_codes()
-	fp = './data_codes/lse_ftse350_stock_codes_scraped_html.txt'
-	with open(fp, 'w') as f:
-		f.write('\n'.join(stock_codes))
-
-	stock_codes = ftse100_stock_codes()
-	fp = './data_codes/lse_ftse100_stock_codes_scraped_html.txt'
-	with open(fp, 'w') as f:
-		f.write('\n'.join(stock_codes))
-
-
-
-
-
-
-
-
+	symbols_output_dir = '../data/symbols'
+	for index in suffix_index_urls.keys():
+		print(index)
+		stock_codes = scrape_stock_symbols(index)
+		fp = os.path.join(symbols_output_dir, '{}_symbols.txt'.format(index))
+		with open(fp, 'w') as f:
+			f.write('\n'.join(stock_codes))
 
